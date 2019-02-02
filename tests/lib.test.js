b3ea047cec19592b9f32d198b8548ee41183234a
Anita Virag <ik.virag@gmail.com>
2019-02-02 19:38:41 +0100

    testing exeptions, continually running tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -56,3 +56,24 @@ describe('getProduct', () => {
     expect(result).toHaveProperty('id', 1);
   })
 });
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    // Null
+    // undefined
+    // NaN
+    // ''
+    // 0
+    // false
+    const args = [null, undefined, NaN, '', 0, false];
+    args.forEach(arg => {
+      expect(() => lib.registerUser(arg)).toThrow();
+    });
+  });
+
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('Josh');
+    expect(result).toMatchObject({username: 'Josh'});
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
