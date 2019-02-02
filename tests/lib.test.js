678058b9c4b578d0a78efe4f4b134ba5696c4bbc
Anita Virag <ik.virag@gmail.com>
2019-02-02 18:58:02 +0100

    grouping tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require('../lib');
 
-test('absolute - should return a positive number if number is positive', () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+describe('absolute', () => {
+  test('should return a positive number if number is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test('absolute - should return a positive number if number is negative', () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+  test('should return a positive number if number is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test('absolute - should return 0 if number is 0', () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  test('should return 0 if number is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
